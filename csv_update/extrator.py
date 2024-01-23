33589964cebf2eaa2c56c80fb46c088d99bf5bd0
fabio-araujo-silva <fabio.kaue.araujo@gmail.com>
2024-01-23 12:27:12 -0300

    extrator funcional

--- csv_update/extrator.py
+++ csv_update/extrator.py
@@ -1,18 +1,20 @@
 import gspread
 import pandas as pd
 
-#busca a planilha
+# Busca a planilha
 client = gspread.service_account(filename='credentials.json')
-planilha = client.open('download')
+planilha = client.open('Registro de Atividades e Frequência_v8_0')
 
-#seleciona todas as células
-dados = planilha.get_worksheet(0)
-dados_completos = dados.get_all_records()
+# Seleciona a planilha desejada (no exemplo, a sexta planilha, índice 5)
+planilha_selecionada = planilha.get_worksheet(5)
 
-# Converte os dados para DF do Pandas
+# Obtém todos os registros da planilha selecionada
+dados_completos = planilha_selecionada.get_all_records()
+
+# Converte os dados para um DataFrame do Pandas
 df = pd.DataFrame(dados_completos)
 
+# Salva o DataFrame em um arquivo CSV
 df.to_csv('../dados/tabela1.csv', index=False)
 
 print("Dados exportados para o arquivo CSV com sucesso.")
-
