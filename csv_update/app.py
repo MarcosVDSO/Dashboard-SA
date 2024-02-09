b8f81c5eb1ad210cc629210a18002c76bc479e6a
Fabio Araujo <82675020+Fabio-araujo-silva@users.noreply.github.com>
2024-02-09 11:32:04 -0300

    atulizacao extrator

--- csv_update/app.py
+++ csv_update/app.py
@@ -1,20 +1,36 @@
 from flask import Flask, request
 import subprocess
+import gspread
+import pandas as pd
 from flask_cors import CORS  
 
 app = Flask(__name__)
 CORS(app, resources={r"/executar": {"origins": "*"}})
 
+client = gspread.service_account(filename='csv_update/credentials.json')
+
 @app.route('/executar', methods=['GET', 'POST'])
 def executar_codigo():
     if request.method == 'POST':
         try:
-            resultado = subprocess.check_output(['python', 'csv_update/extrator.py'])
-            return resultado
+            selected_value = request.form.get('selectedValue')
+            if selected_value == 'Produto 01':
+                resultado = subprocess.check_output(['python', '-c', 'from csv_update.extrator import opcao1; print(opcao1())'])
+                return resultado
+            elif selected_value == 'Produto 02':
+                resultado = subprocess.check_output(['python', '-c', 'from csv_update.extrator import opcao2; print(opcao1())'])
+                return resultado
+            elif selected_value == 'Produto 03':
+                resultado = subprocess.check_output(['python', '-c', 'from csv_update.extrator import opcao3; print(opcao1())'])
+                return resultado
+            else:
+                return "Opção inválida"
+            
         except Exception as e:
             return f"Erro ao executar o código: {e}"
     else:
-        return "Método não permitido. Use POST para acessar este recurso."
+        return "Método não permitido. Use POST para acessar este recurso."   
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- csv_update/extrator.py
+++ csv_update/extrator.py
@@ -4,22 +4,36 @@ import pandas as pd
 
 client = gspread.service_account(filename='csv_update/credentials.json')
 
-planilha_nome = 'Registro de Atividades e Frequência_v8_0'
-planilha = client.open(planilha_nome)
-
-nome_pagina = 'Tabela 1 usada no infografico entrega total'
-
-planilha_selecionada = planilha.worksheet(nome_pagina)
+planilha_nome = 'Planilha_0'
 
 
-dados_completos = planilha_selecionada.get_all_records()
+
+def opcao1():
+    planilha_nome = 'Planilha_0'
+    geraCsv(planilha_nome)
+
+def opcao2():
+    planilha_nome = 'Planilha_1'
+    geraCsv(planilha_nome)
+
+def opcao3():
+    planilha_nome = 'Planilha_2'
+    geraCsv(planilha_nome)
+
+def geraCsv(planilha_nome):
+    planilha = client.open(planilha_nome)
+
+    nome_pagina = 'Tabela 1 usada no infografico entrega total'
+
+    planilha_selecionada = planilha.worksheet(nome_pagina)
 
 
-df = pd.DataFrame(dados_completos)
+    dados_completos = planilha_selecionada.get_all_records()
 
 
-df.to_csv('dados/tabela1.csv', index=False)
-
-print("Dados exportados para o arquivo CSV com sucesso.")
+    df = pd.DataFrame(dados_completos)
 
 
+    df.to_csv('dados/tabela1.csv', index=False)
+
+    print(f"Dados da {planilha_nome} exportados para o arquivo CSV com sucesso.")
